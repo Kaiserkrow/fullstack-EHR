3333834be2884a1179aa066287504cdff6140fbc
Kaiserkrow <42108060+Kaiserkrow@users.noreply.github.com>
2024-03-28 15:07:43 +0800

    login page with styles but no function

--- server.js
+++ server.js
@@ -3,7 +3,17 @@ const app = express();
 
 app.set("view-engine", "ejs");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.render("index.ejs", { name: "judy" });
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
 });
 app.listen(3000);
